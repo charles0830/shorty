ea1f105140f601c3eb74e2d2b1e3af1ffc87de5c
charles0830 <chberry830@gmail.com>
2023-01-11 00:32:59 +0600

    tailwindcss installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,44 +7,12 @@
 
     <title>Shorty</title>
 
-    <!-- Styles -->
-    <style>
-        body {
-            background-color: rgb(31, 41, 55);
-            color: white;
-        }
-    </style>
-
     @vite(['resources/js/app.js'])
 
 </head>
 
-<body class="antialiased">
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-
-        </div>
-    </div>
-
-    <div id="app">
-        <example-component />
-    </div>
+<body class="antialiased bg-gray-900 text-white">
+    <div id="app"></div>
 </body>
 
 </html>
