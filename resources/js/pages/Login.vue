d838645fe2e8893545cef30ecfe6ac077f16a46f
charles0830 <chberry830@gmail.com>
2023-01-13 00:43:11 +0600

    form data binding with submit methods created

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -20,7 +20,7 @@
             </p>
 
             <form
-                @submit.prevent
+                @submit.prevent="login"
                 class="mt-6 mb-0 space-y-4 rounded-lg p-8 shadow-2xl bg-gray-800"
             >
                 <div>
@@ -28,30 +28,12 @@
 
                     <div class="relative mt-1">
                         <input
+                            v-bind="form.email"
                             type="email"
                             id="email"
                             class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
                             placeholder="Enter email"
                         />
-
-                        <span
-                            class="absolute inset-y-0 right-4 inline-flex items-center"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5 text-gray-400"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                                />
-                            </svg>
-                        </span>
                     </div>
                 </div>
 
@@ -62,36 +44,12 @@
 
                     <div class="relative mt-1">
                         <input
+                            v-bind="form.password"
                             type="password"
                             id="password"
                             class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
                             placeholder="Enter password"
                         />
-
-                        <span
-                            class="absolute inset-y-0 right-4 inline-flex items-center"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5 text-gray-400"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                />
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                />
-                            </svg>
-                        </span>
                     </div>
                 </div>
 
@@ -114,7 +72,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            form: {
+                email: "",
+                password: "",
+            },
+        };
+    },
+
+    methods: {
+        login() {
+            console.log("proceed to login the user");
+        },
+    },
+};
 </script>
 
 <style></style>
--- resources/js/pages/Signup.vue
+++ resources/js/pages/Signup.vue
@@ -5,9 +5,9 @@
                 aria-label="Main"
                 class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
             >
-                <div class="max-w-xl lg:max-w-3xl">
+                <div class="max-w-xl">
                     <h1
-                        class="mt-6 text-2xl font-bold text-primary sm:text-3xl lg:text-5xl md:text-4xl"
+                        class="mt-6 text-2xl font-bold text-primary sm:text-3xl md:text-4xl"
                     >
                         Join Shorty,
                         <span class="text-secondary">Save Time</span>, Without
@@ -18,7 +18,10 @@
                         Don't think about it, do it!
                     </p>
 
-                    <form @submit.prevent class="mt-8 grid grid-cols-6 gap-6">
+                    <form
+                        @submit.prevent="signup"
+                        class="mt-8 grid grid-cols-6 gap-6"
+                    >
                         <div class="col-span-6 sm:col-span-3">
                             <label
                                 for="FirstName"
@@ -31,7 +34,7 @@
                                 type="text"
                                 id="FirstName"
                                 name="first_name"
-                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-800 text-md text-gray-300 shadow-sm"
+                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-700 text-md text-gray-300 shadow-sm"
                             />
                         </div>
 
@@ -47,7 +50,7 @@
                                 type="text"
                                 id="LastName"
                                 name="last_name"
-                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-800 text-md text-gray-300 shadow-sm"
+                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-700 text-md text-gray-300 shadow-sm"
                             />
                         </div>
 
@@ -63,7 +66,7 @@
                                 type="email"
                                 id="Email"
                                 name="email"
-                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-800 text-md text-gray-300 shadow-sm"
+                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-700 text-md text-gray-300 shadow-sm"
                             />
                         </div>
 
@@ -79,7 +82,7 @@
                                 type="password"
                                 id="Password"
                                 name="password"
-                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-800 text-md text-gray-300 shadow-sm"
+                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-700 text-md text-gray-300 shadow-sm"
                             />
                         </div>
 
@@ -95,7 +98,7 @@
                                 type="password"
                                 id="PasswordConfirmation"
                                 name="password_confirmation"
-                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-800 text-md text-gray-300 shadow-sm"
+                                class="mt-1 w-full rounded-md border-t-gray-500 border-transparent bg-gray-700 text-md text-gray-300 shadow-sm"
                             />
                         </div>
 
@@ -103,7 +106,7 @@
                             class="col-span-6 sm:flex sm:items-center sm:gap-4"
                         >
                             <button
-                                class="inline-block shrink-0 rounded-md border border-primary bg-primary px-12 py-3 text-md font-medium text-white transition hover:bg-transparent hover:text-primary focus:outline-none focus:ring active:text-primary"
+                                class="inline-block shrink-0 rounded-md border border-primary bg-primary shadow-lg shadow-primary/30 px-12 py-3 text-md font-medium text-white transition hover:bg-transparent hover:text-primary focus:outline-none focus:ring active:text-primary"
                             >
                                 Create an Account
                             </button>
@@ -125,7 +128,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            form: {
+                first_name: "",
+                last_name: "",
+                email: "",
+                password: "",
+                password_confirmation: "",
+            },
+        };
+    },
+
+    methods: {
+        signup() {
+            console.log("proceed to signup");
+        },
+    },
+};
 </script>
 
 <style></style>
