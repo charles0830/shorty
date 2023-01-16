7e13b742e957e4d8ba6f2822109f946fd84b3b08
charles0830 <chberry830@gmail.com>
2023-01-17 00:50:48 +0600

    Copy To Clipboard feature added

--- resources/js/pages/TheIndex.vue
+++ resources/js/pages/TheIndex.vue
@@ -132,8 +132,33 @@
                                         >
                                             <!-- Meta -->
                                             <p
-                                                class="flex flex-shrink-0 gap-2 font-medium text-right text-gray-500 italic space-between"
+                                                class="flex flex-shrink-0 gap-2 font-medium text-right text-gray-500 italic space-between items-center"
                                             >
+                                                <!-- Copy To Clipboard button -->
+                                                <button
+                                                    @click="
+                                                        copyToClipboard(
+                                                            pretty_url(item.short_url)
+                                                        )
+                                                    "
+                                                    class="p-1 text-center transition rounded hover:bg-gray-700"
+                                                >
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke-width="1.5"
+                                                        stroke="currentColor"
+                                                        class="w-6 h-6 text-primary"
+                                                    >
+                                                        <path
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+                                                        />
+                                                    </svg>
+                                                </button>
+
                                                 <span
                                                     v-if="item.visits > 0"
                                                     class="flex gap-1 pr-2 border-r border-r-gray-700"
@@ -266,6 +291,12 @@ export default {
                         }
                     });
             }
+        },
+
+        copyToClipboard(text) {
+            return navigator.clipboard.writeText(text)
+                ? this.$notify("Short URL copied to clipboard")
+                : this.$notify("Oh the misery! Everybody wants to be my enemy");
         },
     },
 
