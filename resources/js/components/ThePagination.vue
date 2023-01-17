570763137b690b81847a830da1ba446aa9d5f1c6
charles0830 <chberry830@gmail.com>
2023-01-17 13:20:23 +0600

    pagination refactored

---
+++ resources/js/components/ThePagination.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="my-8" v-if="items.last_page > 1">
+        <ol
+            class="flex justify-center items-center gap-3 text-sm text-gray-300 font-semibold"
+        >
+            <!-- Previous Button -->
+            <li>
+                <button
+                    @click="$emit('prev')"
+                    class="inline-flex h-8 w-8 items-center justify-center rounded border-transparent"
+                    :class="[
+                        items.current_page === 1
+                            ? 'text-white border-primary bg-primary'
+                            : 'border-y-gray-500 border-y bg-gray-700',
+                    ]"
+                >
+                    <span class="sr-only">Prev Page</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-3 w-3"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <p class="text-gray-300">
+                    Page {{ items.current_page }} of
+                    {{ items.last_page }}
+                </p>
+            </li>
+
+            <!-- Next Button -->
+            <li>
+                <button
+                    @click="$emit('next')" 
+                    class="inline-flex h-8 w-8 items-center justify-center rounded border-transparent"
+                    :class="[
+                        items.current_page === items.last_page
+                            ? 'text-white border-primary bg-primary'
+                            : 'border-y-gray-500 border-y bg-gray-700',
+                    ]"
+                >
+                    <span class="sr-only">Next Page</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-3 w-3"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+            </li>
+        </ol>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        items: {
+            type: Object,
+        },
+    },
+
+    methods: {
+        next() {
+            this.$emit("next");
+        },
+        prev() {
+            this.$emit("prev");
+        },
+    },
+};
+</script>
--- resources/js/pages/TheIndex.vue
+++ resources/js/pages/TheIndex.vue
@@ -212,72 +212,11 @@
                         </div>
 
                         <!-- Pagination -->
-                        <div class="my-8" v-if="items.last_page > 1">
-                            <ol
-                                class="flex justify-center items-center gap-3 text-sm text-gray-300 font-semibold"
-                            >
-                                <!-- Previous Button -->
-                                <li>
-                                    <button
-                                        @click="prev"
-                                        class="inline-flex h-8 w-8 items-center justify-center rounded border-transparent"
-                                        :class="[
-                                            items.current_page === 1
-                                                ? 'text-white border-primary bg-primary'
-                                                : 'border-y-gray-500 border-y bg-gray-700',
-                                        ]"
-                                    >
-                                        <span class="sr-only">Prev Page</span>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            class="h-3 w-3"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                                clip-rule="evenodd"
-                                            />
-                                        </svg>
-                                    </button>
-                                </li>
-                                <li>
-                                    <p class="text-gray-300">
-                                        Page {{ items.current_page }} of
-                                        {{ items.last_page }}
-                                    </p>
-                                </li>
-
-                                <!-- Next Button -->
-                                <li>
-                                    <button
-                                        @click="next"
-                                        class="inline-flex h-8 w-8 items-center justify-center rounded border-transparent"
-                                        :class="[
-                                            items.current_page ===
-                                            items.last_page
-                                                ? 'text-white border-primary bg-primary'
-                                                : 'border-y-gray-500 border-y bg-gray-700',
-                                        ]"
-                                    >
-                                        <span class="sr-only">Next Page</span>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            class="h-3 w-3"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                clip-rule="evenodd"
-                                            />
-                                        </svg>
-                                    </button>
-                                </li>
-                            </ol>
-                        </div>
+                        <ThePagination
+                            :items="items"
+                            @next="next"
+                            @prev="prev"
+                        />
                     </div>
                 </section>
             </div>
@@ -287,6 +226,8 @@
 <script>
 import CopyToClipboard from "../components/buttons/CopyToClipBoard.vue";
 import DeleteUrl from "../components/buttons/DeleteUrl.vue";
+import ThePagination from "../components/ThePagination.vue";
+
 export default {
     middleware: "auth",
 
@@ -406,6 +347,7 @@ export default {
     components: {
         CopyToClipboard,
         DeleteUrl,
+        ThePagination,
     },
 };
 </script>
